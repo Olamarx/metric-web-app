0d0fcfb0854c3c06b5bf24ddeff30fc431edbd87
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-23 16:46:25 +0100

    Download the dependencies

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './components/App';
+import store from './redux/configureStore';
+
+const append = document.querySelector('#root');
+const root = createRoot(append);
+
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+);
