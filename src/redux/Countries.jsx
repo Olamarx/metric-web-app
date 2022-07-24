28a2290720df9c532abcefdfac65b5d722fd8c79
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-24 08:08:53 +0100

    Started the App Creation

--- src/redux/Countries.jsx
+++ src/redux/Countries.jsx
@@ -3,9 +3,10 @@ import axios from 'axios';
 const contries = () => 'https://restcountries.com/v3.1/all';
 
 const Actions = {
-  LOAD: 'details/countries/LOAD',
+  LOAD: 'country/countries/LOAD',
 };
 
+// Reducer
 const initialize = [];
 
 const reducer = (state = initialize, action) => {
@@ -17,6 +18,7 @@ const reducer = (state = initialize, action) => {
   }
 };
 
+// Action Creator
 export const loadAllCountries = () => async (dispatch) => {
   const response = await axios.get(contries());
   const result = await response.data;
@@ -25,22 +27,14 @@ export const loadAllCountries = () => async (dispatch) => {
     const officialName = country.name.official
     const commonName = country.name.common
     const flag = country.flags.png
-    // const continent = country.continents
-    // const coatOfArm = country.coatOfArms.png
-    // const area = country.area
-    // const currencyName = country.area
-    // const currencySymbol = country.currencies.symbol
 
     worldCountries.push({
       officialName,
       commonName,
-      // name,
       flag,
-      // area
-      // country,
     });
   });
-  // console.log(result);
+  // console.log(worldCountries);
   if (response.status === 200) {
     dispatch({
       type: Actions.LOAD,
@@ -51,13 +45,3 @@ export const loadAllCountries = () => async (dispatch) => {
 };
 
 export default reducer;
-
-// export const APIContinent = async () => {
-//   const endPoint = await axios.get('');
-//   console.log(endPoint.data);
-// };
-
-// export const APICountry = async (name) => {
-//   const endPoint = await axios.get(`https://restcountries.com/v2/name/${name}`);
-//   console.log(endPoint.data);
-// };
