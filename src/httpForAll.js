b2a2ae82b4e7e451292a2d0534d42bb46c446f15
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-29 11:21:29 +0100

    changed https to http alone

--- src/httpForAll.js
+++ src/httpForAll.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const countriesPoint = 'https://restcountries.com/v3.1';
+const countriesPoint = 'http://restcountries.com/v3.1';
 const APIkey = '8f40c08c46feff1ed7ccf880a4bb1b09';
 const pollutionPoint = 'http://api.openweathermap.org/data/2.5';
 
